e9fbc256bd12997f20d992021918399a8f5f47a7
Dhasarathan33 <dhass2642@gmail.com>
2026-08-19 22:34:57 +0530

    Configure Tailwind CSS for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
-  preview: {
-    allowedHosts: [
-      "carefree-illumination-production-e062.up.railway.app"
-    ]
-  }
+    plugins: [
+        react(),
+        tailwindcss()
+    ],
+
+    preview: {
+        allowedHosts: [
+            "carefree-illumination-production-e062.up.railway.app"
+        ]
+    }
 });
